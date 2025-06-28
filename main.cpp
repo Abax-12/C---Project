d19f2d3b277ed9513e4cfeb07c20864eb524ef3b
Abdul Raheem <abdulraheemgenius23@gmail.com>
2025-06-29 00:26:05 +0500

    new features

--- main.cpp
+++ main.cpp
@@ -5,6 +5,10 @@ int main(){
     std::cout<<"Hello you are good"<<std::endl;
     std::cout<<"Hello This is me"<<std::endl;
     std::cout<<"Hello There"<<std::endl;
+    std::cout<<"This is enjoyable thing"<<std::endl;
+    std::cout<<"Do you know about anything"<<std::endl;
+    std::cout<<"I am another div"<<std::endl;
+    std::cout<<"HTML"<<std::endl;
     std::cout<<"Hello "<<std::endl;
 
     return 0;
